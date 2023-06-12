f9d5ffcdef1cb151e994d5377acdb72ba7a7a012
kerni2 <kerni2@ya.ru>
2023-06-12 15:03:41 +0300

    add index and show template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
+  root "blog_posts#index"
 end
