e4f2e593a319d162cb5b1d19c80f98ff50a83726
kerni2 <kerni2@ya.ru>
2023-06-16 08:21:36 +0300

    add authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: "user@mail.com").first_or_initialize
+user.update!(
+  password: "111111",
+  password_confirmation: "111111"
+)
